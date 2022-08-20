f0c67d90ffc0f45e40bf85a3f54fa228a709692c
live-sexy <sefsaefsefsefsefsefsef@gmail.com>
2022-08-20 11:06:23 -0700

    yftt15k

---
+++ phishingran_otp.js
@@ -0,0 +1,40 @@
+// My Telegram Channel : @Phishingran_Otp - Coded By : @Owner_Phishing
+
+console.clear();
+
+console.log(
+
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #D32F2F;font-weight: bold;");
